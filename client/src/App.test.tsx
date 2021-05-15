39bf5f607b10f91789f5f4c2670b6d9fb774909c
Chris Wilson <chrisgwilson678@gmail.com>
2021-05-15 19:48:02 -0400

    add enzyme shallow test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import renderer from 'react-test-renderer';
+import Enzyme, {shallow, mount} from 'enzyme';
+
 import App from './App';
+
+
+Enzyme.configure({adapter: new Adapter()})
 
 test('renders App', () => {
   render(<App />);
   const linkElement = screen.getByText("Image Mrkt");
   expect(linkElement).toBeInTheDocument();
 });
+
+it('shallow testing', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('h1').length).toEqual(1);
+})

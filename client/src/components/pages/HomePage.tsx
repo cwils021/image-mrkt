e806e9c6258b7672e034264bdbda3eada6104ddf
Christopher Wilson <chrisgwilson678@gmail.com>
2021-05-19 19:36:14 -0400

    basic client features

--- client/src/components/pages/HomePage.tsx
+++ client/src/components/pages/HomePage.tsx
@@ -4,19 +4,36 @@
 //  TODO User can view buy now buttons for card packs
 //  TODO Cards are added to users inventory
 
-import React from 'react'
-import { Container, Grow, Grid } from '@material-ui/core';
+import React, { useState } from 'react'
+import { Container, Grow, Grid, Button } from '@material-ui/core';
+import axios from 'axios';
+
+
 
 
 const HomePage = () => {
+    const getImage = async () => {
+        const res = await axios.get('https://picsum.photos/500');
+        return res.request.responseURL;
+    }
+
+
+
     return (
         <>
             <h1>HomePage</h1>
             <Grow in>
              <Container>
                  <Grid container direction='row'  justify="space-around" alignItems="center" spacing={3}>
-                    <Grid item xs={12} md={4}><img src="https://picsum.photos/200?random=1" alt="Random"  /></Grid>
-                    <Grid item xs={12} md={4}><img src="https://picsum.photos/200?random=2" alt="Random2" /></Grid>
+                    <Grid container item xs={12} md={4} direction='column' alignItems='center' justify='center'>
+                         <img src="https://picsum.photos/250?random=1" alt="Random"  />
+                        <Button variant='contained' color='primary' onClick={() => {
+                            getImage().then((res) => {alert(res)})
+                        }}>
+                            Buy Now!
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12} md={4}><img src="https://picsum.photos/250?random=2" alt="Random2" /></Grid>
                     <Grid item xs={12} md={4}>Test 3</Grid>
                     <Grid item xs={12} md={4}>Test 4</Grid>
                     <Grid item xs={12} md={4}>Test 5</Grid>
